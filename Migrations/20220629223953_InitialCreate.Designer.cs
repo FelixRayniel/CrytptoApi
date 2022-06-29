1e6bac7d6f24ab149279ed98ae57ac3766dfbabe
Felix Rayniel Reynoso Ventura <felix_reynoso1@ucne.edu.do>
2022-06-29 18:40:51 -0400

    Proyecto culminado, modificando algunas CrypoCoins.

--- Migrations/20220629223953_InitialCreate.Designer.cs
+++ Migrations/20220629223953_InitialCreate.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CryptoApi.Migrations
 {
     [DbContext(typeof(Contexto))]
-    [Migration("20220629223316_InitialCreate")]
+    [Migration("20220629223953_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -43,56 +43,35 @@ namespace CryptoApi.Migrations
                             MonedaId = 1,
                             Descripcion = "Bitcoin",
                             ImageUrl = "https://cdn.icon-icons.com/icons2/674/PNG/512/bitcoin_icon-icons.com_60538.png",
-                            Valor = 20000.610000000001
+                            Valor = 20282.099999999999
                         },
                         new
                         {
                             MonedaId = 2,
-                            Descripcion = "Ethereum",
-                            ImageUrl = "https://cdn.icon-icons.com/icons2/3369/PNG/512/ethereum_coin_money_cryptocurrency_icon_210995.png",
-                            Valor = 1.1739999999999999
-                        },
-                        new
-                        {
-                            MonedaId = 3,
                             Descripcion = "Dogecoin",
                             ImageUrl = "https://cdn.icon-icons.com/icons2/3376/PNG/512/dogecoin_currency_icon_211986.png",
                             Valor = 0.069361000000000006
                         },
                         new
                         {
-                            MonedaId = 4,
-                            Descripcion = "Pax Dollar",
-                            ImageUrl = "https://cdn.icon-icons.com/icons2/2130/PNG/512/pax_cryptocurrency_logo_icon_131411.png",
-                            Valor = 1.0011000000000001
-                        },
-                        new
-                        {
-                            MonedaId = 5,
-                            Descripcion = "EOS",
-                            ImageUrl = "https://cdn.icon-icons.com/icons2/1386/PNG/512/eos-crypto-cryptocurrency-cryptocurrencies-cash-money-bank-payment_95779.png",
-                            Valor = 0.95530000000000004
-                        },
-                        new
-                        {
-                            MonedaId = 6,
+                            MonedaId = 3,
                             Descripcion = "BNB",
                             ImageUrl = "https://cdn.icon-icons.com/icons2/1386/PNG/512/bnb-crypto-cryptocurrency-cryptocurrencies-cash-money-bank-payment_95846.png",
                             Valor = 231.69999999999999
                         },
                         new
                         {
-                            MonedaId = 7,
+                            MonedaId = 4,
                             Descripcion = "XRP",
                             ImageUrl = "https://cdn.icon-icons.com/icons2/2130/PNG/512/xrp_cryptocurrency_logo_icon_131379.png",
                             Valor = 0.34234999999999999
                         },
                         new
                         {
-                            MonedaId = 8,
-                            Descripcion = "Monero",
-                            ImageUrl = "https://cdn.icon-icons.com/icons2/3053/PNG/512/monero_wallet_macos_bigsur_icon_189935.png",
-                            Valor = 117.566
+                            MonedaId = 5,
+                            Descripcion = "Ethereum",
+                            ImageUrl = "https://cdn.icon-icons.com/icons2/3369/PNG/512/ethereum_coin_money_cryptocurrency_icon_210995.png",
+                            Valor = 1.1739999999999999
                         });
                 });
 #pragma warning restore 612, 618
--- Migrations/20220629223953_InitialCreate.cs
+++ Migrations/20220629223953_InitialCreate.cs
@@ -26,42 +26,27 @@ namespace CryptoApi.Migrations
             migrationBuilder.InsertData(
                 table: "Coins",
                 columns: new[] { "MonedaId", "Descripcion", "ImageUrl", "Valor" },
-                values: new object[] { 1, "Bitcoin", "https://cdn.icon-icons.com/icons2/674/PNG/512/bitcoin_icon-icons.com_60538.png", 20000.610000000001 });
+                values: new object[] { 1, "Bitcoin", "https://cdn.icon-icons.com/icons2/674/PNG/512/bitcoin_icon-icons.com_60538.png", 20282.099999999999 });
 
             migrationBuilder.InsertData(
                 table: "Coins",
                 columns: new[] { "MonedaId", "Descripcion", "ImageUrl", "Valor" },
-                values: new object[] { 2, "Ethereum", "https://cdn.icon-icons.com/icons2/3369/PNG/512/ethereum_coin_money_cryptocurrency_icon_210995.png", 1.1739999999999999 });
+                values: new object[] { 2, "Dogecoin", "https://cdn.icon-icons.com/icons2/3376/PNG/512/dogecoin_currency_icon_211986.png", 0.069361000000000006 });
 
             migrationBuilder.InsertData(
                 table: "Coins",
                 columns: new[] { "MonedaId", "Descripcion", "ImageUrl", "Valor" },
-                values: new object[] { 3, "Dogecoin", "https://cdn.icon-icons.com/icons2/3376/PNG/512/dogecoin_currency_icon_211986.png", 0.069361000000000006 });
+                values: new object[] { 3, "BNB", "https://cdn.icon-icons.com/icons2/1386/PNG/512/bnb-crypto-cryptocurrency-cryptocurrencies-cash-money-bank-payment_95846.png", 231.69999999999999 });
 
             migrationBuilder.InsertData(
                 table: "Coins",
                 columns: new[] { "MonedaId", "Descripcion", "ImageUrl", "Valor" },
-                values: new object[] { 4, "Pax Dollar", "https://cdn.icon-icons.com/icons2/2130/PNG/512/pax_cryptocurrency_logo_icon_131411.png", 1.0011000000000001 });
+                values: new object[] { 4, "XRP", "https://cdn.icon-icons.com/icons2/2130/PNG/512/xrp_cryptocurrency_logo_icon_131379.png", 0.34234999999999999 });
 
             migrationBuilder.InsertData(
                 table: "Coins",
                 columns: new[] { "MonedaId", "Descripcion", "ImageUrl", "Valor" },
-                values: new object[] { 5, "EOS", "https://cdn.icon-icons.com/icons2/1386/PNG/512/eos-crypto-cryptocurrency-cryptocurrencies-cash-money-bank-payment_95779.png", 0.95530000000000004 });
-
-            migrationBuilder.InsertData(
-                table: "Coins",
-                columns: new[] { "MonedaId", "Descripcion", "ImageUrl", "Valor" },
-                values: new object[] { 6, "BNB", "https://cdn.icon-icons.com/icons2/1386/PNG/512/bnb-crypto-cryptocurrency-cryptocurrencies-cash-money-bank-payment_95846.png", 231.69999999999999 });
-
-            migrationBuilder.InsertData(
-                table: "Coins",
-                columns: new[] { "MonedaId", "Descripcion", "ImageUrl", "Valor" },
-                values: new object[] { 7, "XRP", "https://cdn.icon-icons.com/icons2/2130/PNG/512/xrp_cryptocurrency_logo_icon_131379.png", 0.34234999999999999 });
-
-            migrationBuilder.InsertData(
-                table: "Coins",
-                columns: new[] { "MonedaId", "Descripcion", "ImageUrl", "Valor" },
-                values: new object[] { 8, "Monero", "https://cdn.icon-icons.com/icons2/3053/PNG/512/monero_wallet_macos_bigsur_icon_189935.png", 117.566 });
+                values: new object[] { 5, "Ethereum", "https://cdn.icon-icons.com/icons2/3369/PNG/512/ethereum_coin_money_cryptocurrency_icon_210995.png", 1.1739999999999999 });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
